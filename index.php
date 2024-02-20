238a64c3be7634242e3aeedf17428eb4a173647c
Charles-Muia <Charles.K.Muia@gmail.com>
2024-02-20 13:15:16 +0300

    Slider_3

--- index.php
+++ index.php
@@ -14,8 +14,100 @@
 <!-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Favicon>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
     <link rel="icon" href="Img/Slider.png">
 
+<!-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<GOOGLE FONTS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 </head>
 <body>
+
+<!-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<HEADER>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+
+    <header>
+        <nav>
+            <a href="">home</a>
+            <a href="">faqs</a>
+            <a href="">services</a>
+        </nav>
+    </header>
+
+<!-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<CAROUSEL>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+
+    <div class="carousel">
+        <div class="list">
+            <div class="item">
+                <img src="Img/img1.jpg" alt="">
+                <div class="content">
+                    <div class="author">Beatrice</div>
+                    <div class="title">Slider Design</div>
+                    <div class="topic"></div>
+                    <div class="description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt omnis rem eaque consectetur ab ipsa excepturi debitis natus fugit iusto!
+                    </div>
+                        <div class="buttons">
+                            <button>view more</button>
+                            <button>view more</button>
+                        </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="Img/img2.jpg" alt="">
+                <div class="content">
+                    <div class="author">Beatrice</div>
+                    <div class="title">Slider Design</div>
+                    <div class="topic"></div>
+                    <div class="description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt omnis rem eaque consectetur ab ipsa excepturi debitis natus fugit iusto!
+                    </div>
+                        <div class="buttons">
+                            <button>view more</button>
+                            <button>view more</button>
+                        </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="Img/img3.jpg" alt="">
+                <div class="content">
+                    <div class="author">Beatrice</div>
+                    <div class="title">Slider Design</div>
+                    <div class="topic"></div>
+                    <div class="description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt omnis rem eaque consectetur ab ipsa excepturi debitis natus fugit iusto!
+                    </div>
+                        <div class="buttons">
+                            <button>view more</button>
+                            <button>view more</button>
+                        </div>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="Img/img4.jpg" alt="">
+                <div class="content">
+                    <div class="author">Beatrice</div>
+                    <div class="title">Slider Design</div>
+                    <div class="topic"></div>
+                    <div class="description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt omnis rem eaque consectetur ab ipsa excepturi debitis natus fugit iusto!
+                    </div>
+                        <div class="buttons">
+                            <button>view more</button>
+                            <button>view more</button>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="slider.js"></script>
     
 </body>
 </html>
