62a5dae56b9c6a00a8e1e554e28ca56bd09a5def
Charles-Muia <Charles.K.Muia@gmail.com>
2024-02-20 10:31:10 +0300

    Slider_1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slider Effect</title>
+
+<!-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<stylesheet>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+    <link rel="stylesheet" href="style.css">
+
+<!-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Favicon>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+    <link rel="icon" href="Img/Slider.png">
+
+</head>
+<body>
+    
+</body>
+</html>
